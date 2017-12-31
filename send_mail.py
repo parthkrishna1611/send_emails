5a9f8e205f7692f403599dfd6f3cd1b111a781f0
Parth Krishna <parthkrishna1611@gmail.com>
2017-12-31 12:10:11 +0530

    Create send_mail.py

---
+++ send_mail.py
@@ -0,0 +1,38 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import config
+
+email_user = config.EMAIL_ID
+email_password = config.PASSWORD
+email_send = config.RECEIPENT_EMAIL
+
+subject = 'Test email'
+
+msg = MIMEMultipart()
+msg['From'] = email_user
+msg['To'] = email_send
+msg['Subject'] = subject
+
+body = 'Hi there, sending this email from Python!'
+msg.attach(MIMEText(body,'plain'))
+
+filename='Fairy Tail Emotional Soundtracks Mix -- WE ARE FAMILY --.3gp'
+attachment  =open(filename,'rb')
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+msg.attach(part)
+text = msg.as_string()
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login(email_user,email_password)
+
+
+server.sendmail(email_user,email_send,text)
+server.quit()
